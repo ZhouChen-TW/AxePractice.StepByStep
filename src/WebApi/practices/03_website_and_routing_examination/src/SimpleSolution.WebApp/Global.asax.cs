1116dfa1c0676f9c36b9f885e97c4edb71da749b
chenzhou <chenzhou@thoughtworks.com>
2017-07-11 22:36:29 +0800

    [chen] Finish 03 website and routing examination.

---
+++ src/WebApi/practices/03_website_and_routing_examination/src/SimpleSolution.WebApp/Global.asax.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Http;
+
+namespace SimpleSolution.WebApp
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            HttpConfiguration httpConfiguration = GlobalConfiguration.Configuration;
+            Bootstrapper.Init(httpConfiguration);
+        }
+    }
+}
---
+++ src/WebApi/practices/03_website_and_routing_examination/src/SimpleSolution.WebApp/UserController.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleSolution.WebApp
+{
+    public class UserController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage GetUserById(long id)
+        {
+            return new HttpRequestMessage().Text(HttpStatusCode.OK, $"get user by id({id})");
+        }
+    }
+}
