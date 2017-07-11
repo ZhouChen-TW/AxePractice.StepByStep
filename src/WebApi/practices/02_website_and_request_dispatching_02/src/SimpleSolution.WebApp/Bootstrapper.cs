bf2c259cfd6f6607f66d0d789a292ff3cc34cfd1
chenzhou <chenzhou@thoughtworks.com>
2017-07-11 22:30:46 +0800

    [chen] Finish 02 website and request dispatching.

--- src/WebApi/practices/02_website_and_request_dispatching_02/src/SimpleSolution.WebApp/Bootstrapper.cs
+++ src/WebApi/practices/02_website_and_request_dispatching_02/src/SimpleSolution.WebApp/Bootstrapper.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Http;
+﻿using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Routing;
 
 namespace SimpleSolution.WebApp
 {
@@ -10,6 +12,35 @@ namespace SimpleSolution.WebApp
             // of our test. So I have create an extension method called
             // Request.Text(HttpStatusCode, string) to help you generating
             // a textual response.
+            configuration.Routes.MapHttpRoute(
+                "get by user dependents",
+                "users/dependents",
+                new {controller = "User", action = "GetByUserDependents" },
+                new {httpMethod = new HttpMethodConstraint(HttpMethod.Get)});
+
+            configuration.Routes.MapHttpRoute(
+                "get user dependents by id",
+                "users/{id}/dependents",
+                new {controller = "User", action = "GetUserDependentsById" },
+                new {httpMethod = new HttpMethodConstraint(HttpMethod.Get), id = @"\d+"});
+
+            configuration.Routes.MapHttpRoute(
+                "get by user name",
+                "users",
+                new {controller = "User", action = "GetByUserName"},
+                new {httpMethod = new HttpMethodConstraint(HttpMethod.Get)});
+
+            configuration.Routes.MapHttpRoute(
+                "get by user id",
+                "users/{id}",
+                new {controller = "User", action = "GetByUserId"},
+                new {httpMethod = new HttpMethodConstraint(HttpMethod.Get), id = @"\d+"});
+
+            configuration.Routes.MapHttpRoute(
+                "update by user id",
+                "users/{id}",
+                new {controller = "User", action = "UpdateByUserId"},
+                new {httpMethod = new HttpMethodConstraint(HttpMethod.Put), id = @"\d+"});
         }
     }
 }
---
+++ src/WebApi/practices/02_website_and_request_dispatching_02/src/SimpleSolution.WebApp/UserController.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleSolution.WebApp
+{
+    public class UserController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage GetByUserDependents()
+        {
+            return new HttpRequestMessage().Text(HttpStatusCode.OK, "This will get all users's dependents");
+        }
+
+        [HttpGet]
+        public HttpResponseMessage GetUserDependentsById(long id)
+        {
+            return new HttpRequestMessage().Text(HttpStatusCode.OK, $"This will get user(id={id})'s dependents");
+        }
+
+        [HttpGet]
+        public HttpResponseMessage GetByUserName([FromUri(Name = "name")]string name)
+        {
+            return new HttpRequestMessage().Text(HttpStatusCode.OK, $"user(name={name}) found");
+        }
+
+        [HttpGet]
+        public HttpResponseMessage GetByUserId(long id)
+        {
+            return new HttpRequestMessage().Text(HttpStatusCode.OK, $"user(id={id}) found");
+        }
+
+        [HttpPut]
+        public HttpResponseMessage UpdateByUserId(long id, [FromUri(Name = "name")]string name)
+        {
+            return new HttpRequestMessage().Text(HttpStatusCode.OK, $"user(id={id})'s name updated to {name}");
+        }
+    }
+}
